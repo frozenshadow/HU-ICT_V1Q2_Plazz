6344f65d7a77001b885a709323b5b7d1c50b84c8
frozenshadow <contact@leijlandia.nl>
2019-01-28 03:07:53 +0100

    Add models and migrations

--- plazz/models.py
+++ plazz/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Form(models.Model):
+    name = models.CharField(max_length=255)
+    enabled = models.BooleanField()
+
+
+class FieldType(models.Model):
+    type = models.CharField(max_length=255)
+
+
+class FormField(models.Model):
+    index = models.IntegerField(default=0)
+    label = models.CharField(max_length=255)
+    required = models.BooleanField()
+    form = models.ForeignKey(Form, unique=True, on_delete=models.CASCADE)
+    fieldtype = models.ForeignKey(FieldType, on_delete=models.CASCADE)
+
+
+class FieldOption(models.Model):
+    index = models.IntegerField(primary_key=True)
+    disabled = models.BooleanField()
+    selected = models.BooleanField()
+    value = models.CharField(max_length=255)
+    formfield = models.ForeignKey(FormField, on_delete=models.CASCADE)
