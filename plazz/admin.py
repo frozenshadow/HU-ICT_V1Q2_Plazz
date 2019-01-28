8eb77fc0a5925f29c237e881bb48909ead9686b3
frozenshadow <contact@leijlandia.nl>
2019-01-28 03:08:29 +0100

    Make model tables editable in admin panel

--- plazz/admin.py
+++ plazz/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Form, FieldType, FormField, FieldOption
+
+admin.site.register([Form, FieldType, FormField, FieldOption])
