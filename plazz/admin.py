fce90bf94e5bb72a2579a6e83d0b2c73ae517f7b
frozenshadow <contact@leijlandia.nl>
2019-01-29 20:09:35 +0100

    Make new tables editable via admin panel

--- plazz/admin.py
+++ plazz/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
-from .models import Form, FieldType, FormField, FieldOption
+from .models import Form, FieldType, FormField, FieldOption, NeighbourhoodCity, Neighbourhood, Location, StateCity, \
+    Street, State, City
 
-admin.site.register([Form, FieldType, FormField, FieldOption])
+admin.site.register([
+    Form,
+    FieldType,
+    FormField,
+    FieldOption,
+    Street,
+    State,
+    City,
+    StateCity,
+    Location,
+    Neighbourhood,
+    NeighbourhoodCity
+])
